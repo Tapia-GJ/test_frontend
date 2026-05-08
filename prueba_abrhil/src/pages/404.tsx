eb5720bd962ab284b4eb13458e9bef0a54ca6148
Luis Tapia <133921246+Tapia-GJ@users.noreply.github.com>
2026-05-07 20:05:11 -0500

    config tailwind y react router

---
+++ prueba_abrhil/src/pages/404.tsx
@@ -0,0 +1,9 @@
+
+export default function NotFoundPage() {
+    return (
+        <div>
+            <h1>404 - Página no encontrada</h1>
+            <p>Lo sentimos, la página que buscas no existe.</p>
+        </div>
+    )
+}
