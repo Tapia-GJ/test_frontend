78285b6a2e0b374bfb227b3eba1136728c10aa58
Luis Tapia <133921246+Tapia-GJ@users.noreply.github.com>
2026-05-09 00:34:36 -0500

    add authService para login

---
+++ prueba_abrhil/src/services/authService.ts
@@ -0,0 +1,11 @@
+import { API_URL } from "@/config/api";
+
+export async function loginRequest(email: string, password: string) {
+  const res = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+
+  return res.json();
+}
