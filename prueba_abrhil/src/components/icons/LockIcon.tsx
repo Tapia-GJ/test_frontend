fbb854b6785c7240d707dbcc2f607e79f5cb93ca
Luis Tapia <133921246+Tapia-GJ@users.noreply.github.com>
2026-05-08 23:16:04 -0500

    add componentes icons, se intalaron dependencias para login

---
+++ prueba_abrhil/src/components/icons/LockIcon.tsx
@@ -0,0 +1,18 @@
+export const LockIcon = ({ className = "w-6 h-6", ...props }: React.SVGProps<SVGSVGElement>) => (
+
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 20 20"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={className}
+        {...props}
+    >
+        <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+    </svg>
+);
