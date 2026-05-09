f623ace67167407bb6ddc9e2664c40aad366dafe
Luis Tapia <133921246+Tapia-GJ@users.noreply.github.com>
2026-05-09 00:37:29 -0500

    add logout

--- prueba_abrhil/src/components/Header.tsx
+++ prueba_abrhil/src/components/Header.tsx
@@ -1,17 +1,26 @@
 import { Link } from "react-router";
-
+import { useAuthStore } from "@/store/authStore";
+import { useNavigate } from "react-router";
 export default function Header() {
+    const navigate = useNavigate();
+    const { logout } = useAuthStore();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/login");
+    };
+
     return (
-        <nav className="px-4 py-3 bg-gray-100 sticky top-0 z-10 lg:flex lg:items-stretch w-full">
+        <nav className="px-4 py-3 bg-gray-100 sticky top-0 z-10 md:flex md:items-stretch w-full">
             <div className="flex flex-no-shrink items-stretch h-12">
                 <a href="/" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-gray-900 no-underline flex items-center text-2xl">Prueba Tecnica Abrhil</a>
             </div>
-            <div className="lg:flex lg:items-stretch lg:flex-no-shrink lg:grow">
-                <div className="lg:flex lg:items-stretch lg:justify-end ml-auto gap-2">
+            <div className="md:flex md:items-stretch md:flex-no-shrink md:grow">
+                <div className="md:flex md:items-stretch md:justify-end ml-auto gap-2">
                     <Link to="/users" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-gray-900 no-underline flex items-center hover:bg-gray-300 rounded-xl">Usuarios</Link>
                     <Link to="/profile" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-gray-900 no-underline flex items-center hover:bg-gray-300 rounded-xl">Perfil</Link>
                     <Link to="/admin/audit" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-gray-900 no-underline flex items-center hover:bg-gray-300 rounded-xl">Auditar</Link>
-                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg">
+                    <button onClick={handleLogout} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg">
                         Cerrar sesión
                     </button>
                 </div>
