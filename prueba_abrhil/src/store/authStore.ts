f186f5478fd1699101dac1ca395be001207a56f9
Luis Tapia <133921246+Tapia-GJ@users.noreply.github.com>
2026-05-09 00:34:06 -0500

    add authStore con persistencia

---
+++ prueba_abrhil/src/store/authStore.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import { type User } from "@/types/user";
+import { persist } from "zustand/middleware";
+interface AuthState {
+  user: User | null;
+  token: string | null;
+  setAuth: (user: User, token: string) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      setAuth: (user, token) => set({ user, token }),
+      logout: () => set({ user: null, token: null }),
+    }),
+    { name: "auth-storage" },
+  ),
+);
